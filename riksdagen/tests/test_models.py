944bf5ac7960b161708d9b2b160a0c01fe53fac1
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2014-09-09 15:17:20 +0200

    Passing tests for all changes in riksdagen

--- riksdagen/tests/test_models.py
+++ riksdagen/tests/test_models.py
@@ -8,7 +8,8 @@ class VotingAggTest(TestCase):
 
     def setUp(self):
         self.d = DocumentFactory()
-        VotingFactory.create_batch(50, document=self.d)
+        VotingFactory.create_batch(50, document=self.d, vote='Ja')
+        VotingFactory.create_batch(25, document=self.d, vote='Nej')
 
     def test_votingagg_gets_created(self):
         count = VotingAgg.objects.all().count()
@@ -17,4 +18,11 @@ class VotingAggTest(TestCase):
         self.d.save()
         agg = VotingAgg.objects.all()
         self.assertEqual(agg.count(), 1)
+
+    def test_votingagg_contains_right_amount_aggregate(self):
+        self.d.save()
+        agg = VotingAgg.objects.all().first()
+
+        self.assertEqual(agg.q1_yes, 50)
+        self.assertEqual(agg.q1_no, 25)
 
