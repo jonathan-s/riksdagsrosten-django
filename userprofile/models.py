24be628d67551075986207e312b1edd74e9b8b11
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2014-09-09 15:27:14 +0200

    Moving logic from signals to tasks

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,15 +1,13 @@
 import datetime
 
 from django.db import models
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from django.dispatch import receiver
 from allauth.account.models import EmailAddress
 
 from riksdagen.models import VotingBase
-from riksdagen.models import Voting
 from riksdagen.models import Person
 from riksdagen.models import Document
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='profile')
@@ -53,47 +51,3 @@ class UserSimilarity(models.Model):
     def __str__(self):
         return "{0} vs {1}: {2}%".format(
             self.user, self.mp, self.percentage)
-
-def update_or_create(updated_values):
-    instance, created = UserSimilarity.objects.get_or_create(**updated_values)
-    if created:
-        return created
-    else:
-        UserSimilarity.objects.update(**updated_values)
-        return False
-
-@receiver(post_save, sender=UserVote)
-def update_person(sender, instance, created, raw, using, update_fields, **kwargs):
-    count = UserVote.objects.filter(user=instance.user).count()
-    UserProfile.objects.filter(user__id=instance.user.id).update(
-        nr_votes=count, last_voted_on=datetime.datetime.now())
-
-    mps = Person.objects.filter(
-            commitments__until=datetime.date(2014, 9, 29),
-            commitments__role_code__exact='Riksdagsledamot')
-    votes = UserVote.objects.filter(user=instance.user)
-
-    for mp in mps:
-        d = {}
-        total_votes = 0
-        # getting all similar votes.
-        vote_sum = 0
-        for v in votes:
-            try:
-                mp_vote = mp.votes.get(voting_id=v.voting_id).vote
-                if mp_vote == v.vote:
-                    vote_sum += 1
-                if mp_vote == 'Ja' or mp_vote == 'Nej':
-                    total_votes += 1
-            except Voting.DoesNotExist:
-                print('Skipping this vote!')
-
-        d = {
-            'user': instance.user,
-            'percentage': (vote_sum/total_votes)*100,
-            'common_votes': total_votes,
-            'mp': mp
-            }
-        update_or_create(d)
-
-
