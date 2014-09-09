a0d1f071712689b8f9fc9a671a48ce2a9494bb15
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2014-09-09 15:16:19 +0200

    Adding poll_detail

--- riksdagsrosten/urls.py
+++ riksdagsrosten/urls.py
@@ -1,21 +1,26 @@
+# encodnig: utf-8
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from riksdagen.views import party, partywithname, singlemp, allmp
-from riksdagen.views import polls, home
+from riksdagen.views import polls, home, poll_detail
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^', include('userprofile.urls')),
     url(r'^$', home, name='home'),
     url(r'^parti/$', party, name='party'),
     url(r'^parti/(?P<partyname>\w+)/$', partywithname, name='partywithname'),
     url(r'^ledamot/(?P<mp_id>\d+)/(?P<nameslug>[-\w]+)/$', singlemp, name='singlemp'),
     url(r'^ledamot/(?P<mp_id>\d+)/$', singlemp, name='mpredirect'),
     url(r'^ledamot/$', allmp, name='allmp'),
+    url(r'^votering/(?P<doc_id>[A-Öa-ö0-9]+)/$', poll_detail, name='poll_detail'),
     url(r'^votering/tidigare/kategori/(?P<category>\w+)/$', polls, name='pollscat'),
     url(r'^votering/tidigare/kategori/$', polls, name='polls'),
     url(r'^accounts/', include('allauth.urls')),
+
     # Examples:
     # url(r'^$', 'riksdagsrosten.views.home', name='home'),
     # url(r'^riksdagsrosten/', include('riksdagsrosten.foo.urls')),
