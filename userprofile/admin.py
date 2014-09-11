e55560cfa6917232273099492a564e6aed685c23
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2014-09-11 12:44:17 +0200

    Adding basic admin information to admin-backend

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile
+from .models import UserVote
+from .models import UserSimilarity
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+class UserVoteAdmin(admin.ModelAdmin):
+    pass
+
+class UserSimilarityAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UserVote, UserVoteAdmin)
+admin.site.register(UserSimilarity, UserSimilarityAdmin)
