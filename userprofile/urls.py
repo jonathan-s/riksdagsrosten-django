fb755007b78d3a7f3ffdc4d1fad7102d995c575f
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2014-09-22 13:27:54 +0200

    Provide my own user logout that redirects to start page

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -6,12 +6,12 @@ from userprofile.views import userprofile
 from userprofile.views import openprofile
 from userprofile.views import user_settings
 from userprofile.views import poll_detail_vote
-from userprofile.views import logout
+from userprofile.views import user_logout
 
 
 urlpatterns = patterns('',
     url(r'^profil/$', userprofile, name='userprofile'),
-    url(r'^profil/logout/$', logout, name='logout'),
+    url(r'^profil/logout/$', user_logout, name='logout'),
     url(r'^profil/installningar/$', user_settings, name='usersettings'),
     url(r'^anvandare/(?P<username>\w+)/$', openprofile, name='openprofile'),
     url(r'^votering/(?P<doc_id>[A-Öa-ö0-9]+)/(?P<doc_item>\d+)/(?P<vote>Ja|Nej)/$',
