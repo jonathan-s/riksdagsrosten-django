10ac812dc4d30e388a70184a0fd20a689dab4f15
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2014-09-25 16:42:10 +0200

    Passing tests for similarity in single_mp view

--- riksdagen/tests/test_views.py
+++ riksdagen/tests/test_views.py
@@ -7,6 +7,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.core.management import call_command
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 from .factories import PersonFactory, PersonWithCommitment
 from .factories import VotingAggFactory
@@ -14,6 +15,8 @@ from riksdagen.constants import GOVORGAN, PARTY_NAME
 from riksdagen.views import party, partywithname, singlemp
 from riksdagen.views import allmp, polls, poll_detail
 from riksdagen.models import Person, VotingAgg
+
+from userprofile.models import UserSimilarity
 
 """
 
@@ -120,7 +123,7 @@ class PartyWithNameTest(TestCase):
 class SingleMpTest(TestCase):
 
     def setUp(self):
-        self.person = PersonFactory()
+        self.mp = PersonFactory()
 
     def test_singlemp_resolves_url_correct(self):
         found = resolve('/ledamot/123123/name-here/')
@@ -130,13 +133,13 @@ class SingleMpTest(TestCase):
         self.assertEqual(found2.func, singlemp)
 
     def test_singlemp_redirects_correctly(self):
-        response = self.client.get('/ledamot/%s/' % self.person.intressent_id)
+        response = self.client.get('/ledamot/%s/' % self.mp.intressent_id)
 
         self.assertEqual(response.status_code, 301)
 
     def test_singlemp_has_correct_template(self):
-        idnr = self.person.intressent_id
-        nameslug = "{0}-{1}".format(self.person.firstname, self.person.lastname)
+        idnr = self.mp.intressent_id
+        nameslug = "{0}-{1}".format(self.mp.firstname, self.mp.lastname)
         response = self.client.get('/ledamot/{0}/{1}/'.format(idnr, nameslug))
 
         self.assertTemplateUsed(response, 'mp.html')
@@ -148,10 +151,27 @@ class SingleMpTest(TestCase):
 
     def test_singlemp_contains_right_mp(self):
         response = self.client.get(
-            '/ledamot/{0}'.format(self.person.intressent_id), follow=True)
+            '/ledamot/{0}'.format(self.mp.intressent_id), follow=True)
 
-        mp = get_object_or_404(Person, pk=self.person.intressent_id)
+        mp = get_object_or_404(Person, pk=self.mp.intressent_id)
         self.assertEqual(response.context['mp'], mp)
+
+    def test_single_mp_shows_similarity_when_logged_in(self):
+        user = User.objects.create_user(username='joe', password='pass')
+        self.client.login(username='joe', password='pass')
+        UserSimilarity.objects.create(
+            user=user,
+            percentage=87.34,
+            common_votes=1,
+            mp=self.mp)
+        response = self.client.get(
+            '/ledamot/{}'.format(self.mp.intressent_id), follow=True)
+
+        self.assertEqual(float(response.context['similarity'].percentage), 87.34)
+
+    def test_single_mp_similarity_not_logged_in(self):
+        pass # what to do.
+
 
 class AllMpTest(TestCase):
 
